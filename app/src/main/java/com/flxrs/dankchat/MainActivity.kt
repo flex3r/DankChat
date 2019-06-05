d6dfdfaf172a281083d6580a17bb3090fb90f4d0
Felix Schmid <felixschmidbb@gmail.com>
2019-06-05 22:40:26 +0200

    imagine being to dumb to paste the correct secret

--- app/src/main/java/com/flxrs/dankchat/MainActivity.kt
+++ app/src/main/java/com/flxrs/dankchat/MainActivity.kt
@@ -18,7 +18,9 @@ import com.flxrs.dankchat.utils.AddChannelDialogFragment
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -33,8 +35,7 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
-		AppCenter.start(application, "2be94b1a-8bd0-447d-8e06-db527eb6e944",
-				Crashes::class.java)
+		AppCenter.start(application, "067a0d4f-9e69-4ffd-9b46-0c5ccb2843a8", Analytics::class.java, Distribute::class.java, Crashes::class.java)
 
 		authStore = TwitchAuthStore(this)
 		val oauth = authStore.getOAuthKey() ?: ""
