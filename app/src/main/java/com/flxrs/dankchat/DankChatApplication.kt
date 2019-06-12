45383abe74c7323eac8c775fcd29f40849ac8998
flex3r <felixschmidbb@gmail.com>
2019-06-13 00:25:53 +0200

    fixed font getting bundled into apk

--- app/src/main/java/com/flxrs/dankchat/DankChatApplication.kt
+++ app/src/main/java/com/flxrs/dankchat/DankChatApplication.kt
@@ -1,8 +1,9 @@
 package com.flxrs.dankchat
 
 import android.app.Application
-import androidx.emoji.bundled.BundledEmojiCompatConfig
+import androidx.core.provider.FontRequest
 import androidx.emoji.text.EmojiCompat
+import androidx.emoji.text.FontRequestEmojiCompatConfig
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -13,14 +14,21 @@ class DankChatApplication : Application() {
 		super.onCreate()
 		startKoin {
 			androidLogger()
-			androidContext(this@DankChatApplication)
+			androidContext(applicationContext)
 
 			modules(appModules)
 		}
 
 		AndroidThreeTen.init(this)
 
-		val config = BundledEmojiCompatConfig(this)
+		val request = FontRequest(
+				"com.google.android.gms.fonts",
+				"com.google.android.gms",
+				"Noto Color Emoji Compat",
+				R.array.com_google_android_gms_fonts_certs
+		)
+		val config = FontRequestEmojiCompatConfig(applicationContext, request)
 		EmojiCompat.init(config)
+
 	}
 }
