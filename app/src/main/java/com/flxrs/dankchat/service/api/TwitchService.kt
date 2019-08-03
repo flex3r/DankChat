cb92c2201aa80f913cb884882a21e41eb86343e1
Felix Schmid <felixschmidbb@gmail.com>
2019-08-03 22:18:30 +0200

    add user-agent to network calls, fixes #22

--- app/src/main/java/com/flxrs/dankchat/service/api/TwitchService.kt
+++ app/src/main/java/com/flxrs/dankchat/service/api/TwitchService.kt
@@ -1,5 +1,6 @@
 package com.flxrs.dankchat.service.api
 
+import com.flxrs.dankchat.BuildConfig
 import com.flxrs.dankchat.service.api.model.BadgeEntities
 import com.flxrs.dankchat.service.api.model.EmoteEntities
 import com.flxrs.dankchat.service.api.model.RecentMessages
@@ -12,37 +13,60 @@ interface TwitchService {
 
     @Headers(
         "Accept: application/vnd.twitchtv.v5+json",
-        "Client-ID: ${TwitchApi.CLIENT_ID}"
+        "Client-ID: ${TwitchApi.CLIENT_ID}",
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
     )
     @GET("user")
     suspend fun getUser(@Header("Authorization") oauth: String): Response<UserEntities.FromKraken>
 
     @Headers(
         "Accept: application/vnd.twitchtv.v5+json",
-        "Client-ID: ${TwitchApi.CLIENT_ID}"
+        "Client-ID: ${TwitchApi.CLIENT_ID}",
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
     )
     @GET("users/{id}/emotes")
     suspend fun getUserEmotes(@Header("Authorization") oauth: String, @Path("id") userId: Int): Response<EmoteEntities.Twitch.Result>
 
-    @Headers("Client-ID: ${TwitchApi.CLIENT_ID}")
+    @Headers(
+        "Client-ID: ${TwitchApi.CLIENT_ID}",
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
+    )
     @GET
     suspend fun getUserHelix(@Url url: String): Response<UserEntities.FromHelixAsArray>
 
+    @Headers(
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
+    )
     @GET
     suspend fun getBadgeSets(@Url url: String): Response<BadgeEntities.Result>
 
+    @Headers(
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
+    )
     @GET
     suspend fun getFFZChannelEmotes(@Url url: String): Response<EmoteEntities.FFZ.Result>
 
+    @Headers(
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
+    )
     @GET
     suspend fun getFFZGlobalEmotes(@Url url: String): Response<EmoteEntities.FFZ.GlobalResult>
 
+    @Headers(
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
+    )
     @GET
     suspend fun getBTTVChannelEmotes(@Url url: String): Response<EmoteEntities.BTTV.Result>
 
+    @Headers(
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
+    )
     @GET
     suspend fun getBTTVGlobalEmotes(@Url url: String): Response<EmoteEntities.BTTV.GlobalResult>
 
+    @Headers(
+        "User-Agent: dankchat/${BuildConfig.VERSION_NAME}"
+    )
     @GET
     suspend fun getRecentMessages(@Url url: String): Response<RecentMessages>
 }
