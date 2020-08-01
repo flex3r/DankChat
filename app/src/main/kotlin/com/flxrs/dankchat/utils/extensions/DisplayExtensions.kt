285b1aff6e1dd0c0211c7a36a207ea17bf512788
Felix <felixschmidbb@gmail.com>
2020-08-01 18:14:13 +0200

    Add dp/px extension properties

---
+++ app/src/main/kotlin/com/flxrs/dankchat/utils/extensions/DisplayExtensions.kt
@@ -0,0 +1,9 @@
+package com.flxrs.dankchat.utils.extensions
+
+import android.content.res.Resources
+
+val Int.dp: Int
+    get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+
+val Int.px: Int
+    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
