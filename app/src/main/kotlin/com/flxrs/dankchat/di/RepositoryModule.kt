a7b9bea6d80623d8cfbbeeecf862c206540dd4ae
Felix <felixschmidbb@gmail.com>
2020-08-19 18:36:39 +0200

    Update dependencies

--- app/src/main/kotlin/com/flxrs/dankchat/di/RepositoryModule.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/di/RepositoryModule.kt
@@ -5,10 +5,10 @@ import com.flxrs.dankchat.service.DataRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
 object RepositoryModule {
 
