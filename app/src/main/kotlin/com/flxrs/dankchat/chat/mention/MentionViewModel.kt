158b5ba2202bf95953e907f75ce50cfa6c1e6a7d
Felix <felixschmidbb@gmail.com>
2020-11-17 14:38:07 +0100

    Remove another unused parameter

--- app/src/main/kotlin/com/flxrs/dankchat/chat/mention/MentionViewModel.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/chat/mention/MentionViewModel.kt
@@ -1,22 +1,20 @@
 package com.flxrs.dankchat.chat.mention
 
 import android.util.Log
-import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.flxrs.dankchat.chat.ChatItem
 import com.flxrs.dankchat.service.ChatRepository
 import kotlinx.coroutines.CoroutineExceptionHandler
 
-class MentionViewModel @ViewModelInject constructor(@Assisted savedStateHandle: SavedStateHandle, repository: ChatRepository) : ViewModel() {
+class MentionViewModel @ViewModelInject constructor(chatRepository: ChatRepository) : ViewModel() {
     private val coroutineExceptionHandler = CoroutineExceptionHandler { _, t ->
         Log.e(TAG, Log.getStackTraceString(t))
     }
-    val mentions: LiveData<List<ChatItem>> = repository.mentions.asLiveData(coroutineExceptionHandler)
-    val whispers: LiveData<List<ChatItem>> = repository.whispers.asLiveData(coroutineExceptionHandler)
+    val mentions: LiveData<List<ChatItem>> = chatRepository.mentions.asLiveData(coroutineExceptionHandler)
+    val whispers: LiveData<List<ChatItem>> = chatRepository.whispers.asLiveData(coroutineExceptionHandler)
 
     companion object {
         private val TAG = MentionViewModel::class.java.simpleName
