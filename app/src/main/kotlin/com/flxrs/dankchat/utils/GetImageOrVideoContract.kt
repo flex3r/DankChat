988dfb326c2fbdcc3fd77a96f8bbccabf983b87a
Felix <felixschmidbb@gmail.com>
2021-10-31 21:41:09 +0100

    Update dependencies

--- app/src/main/kotlin/com/flxrs/dankchat/utils/GetImageOrVideoContract.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/utils/GetImageOrVideoContract.kt
@@ -7,7 +7,7 @@ import android.net.Uri
 import androidx.activity.result.contract.ActivityResultContract
 
 class GetImageOrVideoContract : ActivityResultContract<Unit, Uri?>() {
-    override fun createIntent(context: Context, input: Unit?): Intent {
+    override fun createIntent(context: Context, input: Unit): Intent {
         return Intent(Intent.ACTION_GET_CONTENT)
             .addCategory(Intent.CATEGORY_OPENABLE)
             .setType("*/*")
