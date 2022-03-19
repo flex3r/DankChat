2e12da0e28cb0d9ada9c6ea4f90514bb5e733adb
Felix <felixschmidbb@gmail.com>
2022-03-19 17:34:47 +0100

    Update translations

--- app/src/main/kotlin/com/flxrs/dankchat/preferences/ui/ChatSettingsFragment.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/preferences/ui/ChatSettingsFragment.kt
@@ -3,7 +3,6 @@ package com.flxrs.dankchat.preferences.ui
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
