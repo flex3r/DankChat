1f94876c2f54644493bcfa5e0ab5a9e6c337573c
Rodchananat K <rod8711@gmail.com>
2022-08-09 14:53:30 +0700

    revalidate username before connecting to chat

--- app/src/main/kotlin/com/flxrs/dankchat/DankChatViewModel.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/DankChatViewModel.kt
@@ -1,14 +1,20 @@
 package com.flxrs.dankchat
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.flxrs.dankchat.data.ChatRepository
 import com.flxrs.dankchat.data.DataRepository
+import com.flxrs.dankchat.data.api.ApiManager
+import com.flxrs.dankchat.preferences.DankChatPreferenceStore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DankChatViewModel @Inject constructor(
     private val chatRepository: ChatRepository,
+    private val dankChatPreferenceStore: DankChatPreferenceStore,
+    private val apiManager: ApiManager,
     dataRepository: DataRepository,
 ) : ViewModel() {
 
@@ -21,7 +27,13 @@ class DankChatViewModel @Inject constructor(
             chatRepository.reconnectIfNecessary()
         } else {
             started = true
-            chatRepository.connectAndJoin(name, oAuth, channels)
+            viewModelScope.launch {
+                val token = oAuth.substring(6) // remove the "oauth:" prefix
+                val result = apiManager.validateUser(token)
+                val nameToUpdate = result?.login ?: name // fallback to old name if oAuth fail
+                dankChatPreferenceStore.userName = nameToUpdate
+                chatRepository.connectAndJoin(nameToUpdate, oAuth, channels)
+            }
         }
     }
 
