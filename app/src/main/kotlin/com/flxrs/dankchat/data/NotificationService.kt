6c929511be3ad87ecb1780f94ade68f3167284b7
Robmart <robmart@users.noreply.github.com>
2022-08-24 17:55:07 +0200

    Match all emoji

--- app/src/main/kotlin/com/flxrs/dankchat/data/NotificationService.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/data/NotificationService.kt
@@ -277,6 +277,7 @@ class NotificationService : Service(), CoroutineScope {
                     ttsMessage = ttsMessage.replace(emote.code, "", ignoreCase = true)
                 }
             }
+            ttsMessage = ttsMessage.replace(Regex("(\\u00a9|\\u00ae|[\\u2000-\\u3300]|\\ud83c[\\ud000-\\udfff]|\\ud83d[\\ud000-\\udfff]|\\ud83e[\\ud000-\\udfff])"), "")
         }
         tts?.speak(ttsMessage, queueMode, null, null)
     }
