b422fcc8df0a1efb16c6d77c477de2ac074775fa
Felix <felixschmidbb@gmail.com>
2022-10-01 19:58:03 +0200

    Clear old emote usages at app start

--- app/src/main/kotlin/com/flxrs/dankchat/data/repo/EmoteUsageRepository.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/data/repo/EmoteUsageRepository.kt
@@ -1,13 +1,27 @@
 package com.flxrs.dankchat.data.repo
 
+import android.util.Log
 import com.flxrs.dankchat.data.database.EmoteUsageDao
 import com.flxrs.dankchat.data.database.entity.EmoteUsageEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import java.time.Instant
 import javax.inject.Inject
 
 class EmoteUsageRepository @Inject constructor(
-    private val emoteUsageDao: EmoteUsageDao
+    private val emoteUsageDao: EmoteUsageDao,
+    private val coroutineScope: CoroutineScope
 ) {
+
+    init {
+        coroutineScope.launch {
+            runCatching {
+                emoteUsageDao.deleteOldUsages()
+            }
+        }
+    }
+
     suspend fun addEmoteUsage(emoteId: String) {
         val entity = EmoteUsageEntity(
             emoteId = emoteId,
