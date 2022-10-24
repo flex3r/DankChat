b90b1d603bb1501750167e3df5a4c9b4be06ec22
road <rod8711@gmail.com>
2022-10-24 23:55:06 +0700

    fix android < 13 version checking

--- app/src/main/kotlin/com/flxrs/dankchat/preferences/upload/RecentUploadsAdapter.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/preferences/upload/RecentUploadsAdapter.kt
@@ -36,7 +36,7 @@ class RecentUploadsAdapter : ListAdapter<RecentUpload, RecentUploadsAdapter.Uplo
                 val context = it.context
                 context.getSystemService<ClipboardManager>()?.setPrimaryClip(clipData)
                 // show copied toast only on Android < 13
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S_V2) {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2) {
                     Toast.makeText(context, context.getString(R.string.copied_image_url), Toast.LENGTH_SHORT).show()
                 }
             }
