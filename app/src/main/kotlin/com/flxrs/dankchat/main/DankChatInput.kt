271c377b2b5984c2606b94d59a2dba88dd105b99
Felix <felixschmidbb@gmail.com>
2022-10-24 22:41:11 +0200

    Add back old way of clearing keyboard focus for SDK < 23

--- app/src/main/kotlin/com/flxrs/dankchat/main/DankChatInput.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/main/DankChatInput.kt
@@ -1,7 +1,9 @@
 package com.flxrs.dankchat.main
 
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
+import android.view.KeyEvent
 import android.widget.AdapterView
 import androidx.appcompat.widget.AppCompatMultiAutoCompleteTextView
 import com.flxrs.dankchat.chat.suggestion.SuggestionsArrayAdapter
@@ -11,6 +13,15 @@ class DankChatInput : AppCompatMultiAutoCompleteTextView {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onKeyPreIme(keyCode: Int, event: KeyEvent?): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M && event?.keyCode == KeyEvent.KEYCODE_BACK) {
+            clearFocus()
+        }
+
+        return super.onKeyPreIme(keyCode, event)
+    }
+
 
     fun setSuggestionAdapter(enabled: Boolean, adapter: SuggestionsArrayAdapter) {
         if (enabled) {
