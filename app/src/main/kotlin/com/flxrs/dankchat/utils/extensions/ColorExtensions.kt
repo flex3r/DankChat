6e78a76b1140a4239e6104c4dfe0f106d3eeaeed
Felix <felixschmidbb@gmail.com>
2022-11-02 21:59:40 +0100

    Normalize user colors based on background lightness

--- app/src/main/kotlin/com/flxrs/dankchat/utils/extensions/ColorExtensions.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/utils/extensions/ColorExtensions.kt
@@ -7,32 +7,40 @@ import com.google.android.material.color.MaterialColors
 import kotlin.math.sin
 
 @ColorInt
-fun Int.normalizeColor(isDarkMode: Boolean): Int {
+fun Int.normalizeColor(@ColorInt background: Int): Int {
 
+    val isLightBackground = MaterialColors.isColorLight(background)
     val hsl = FloatArray(3)
     ColorUtils.colorToHSL(this, hsl)
     val huePercentage = hsl[0] / 360f
-    if (isDarkMode) {
-        if (hsl[2] < 0.5f) {
-            hsl[2] = 0.5f
+
+    return when {
+        isLightBackground -> {
+            if (hsl[2] > 0.5f) {
+                hsl[2] = 0.5f
+            }
+
+            if (hsl[2] > 0.4f && huePercentage > 0.1f && huePercentage < 0.33333f) {
+                hsl[2] = (hsl[2] -
+                        sin((huePercentage - 0.1f) / (0.33333f - 0.1f) * 3.14159f) * hsl[1] * 0.4f)
+            }
+
+            ColorUtils.HSLToColor(hsl)
         }
 
-        if (hsl[2] < 0.6f && huePercentage > 0.54444f && huePercentage < 0.83333f) {
-            hsl[2] = (hsl[2] +
-                    sin((huePercentage - 0.54444f) / (0.83333f - 0.54444f) * 3.14159f) * hsl[1] * 0.4f)
+        else              -> {
+            if (hsl[2] < 0.5f) {
+                hsl[2] = 0.5f
+            }
+
+            if (hsl[2] < 0.6f && huePercentage > 0.54444f && huePercentage < 0.83333f) {
+                hsl[2] = (hsl[2] +
+                        sin((huePercentage - 0.54444f) / (0.83333f - 0.54444f) * 3.14159f) * hsl[1] * 0.4f)
+            }
+
+            ColorUtils.HSLToColor(hsl)
         }
-
-        return ColorUtils.HSLToColor(hsl)
     }
-    if (hsl[2] > 0.5f) {
-        hsl[2] = 0.5f
-    }
-
-    if (hsl[2] > 0.4f && huePercentage > 0.1f && huePercentage < 0.33333f) {
-        hsl[2] = (hsl[2] -
-                sin((huePercentage - 0.1f) / (0.33333f - 0.1f) * 3.14159f) * hsl[1] * 0.4f)
-    }
-    return ColorUtils.HSLToColor(hsl)
 }
 
 @ColorInt
