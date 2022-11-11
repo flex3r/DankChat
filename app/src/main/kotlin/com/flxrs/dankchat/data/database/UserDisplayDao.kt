a57a5db2ee7320de449921cca707122223898798
road <rod8711@gmail.com>
2022-11-11 20:42:44 +0700

    update call signature and make data access suspend

--- app/src/main/kotlin/com/flxrs/dankchat/data/database/UserDisplayDao.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/data/database/UserDisplayDao.kt
@@ -1,18 +1,15 @@
 package com.flxrs.dankchat.data.database
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface UserDisplayDao {
     @Query("SELECT * from user_display")
-    fun getAll(): List<UserDisplayEntity>
+    suspend fun getAll(): List<UserDisplayEntity>
 
-    @Insert
-    fun insertAll(vararg users: UserDisplayEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(users: List<UserDisplayEntity>)
 
     @Delete
-    fun delete(user: UserDisplayEntity): Int
+    suspend fun deleteAll(user: List<UserDisplayEntity>): Int
 }
