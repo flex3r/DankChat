8fdada8103d45960b3379f9ec0c1c0f563677ed0
M4tthewDE <mattider2@gmail.com>
2022-11-22 21:41:42 +0100

    Add global user state message test

--- app/src/test/kotlin/com/flxrs/dankchat/data/irc/IrcMessageTest.kt
+++ app/src/test/kotlin/com/flxrs/dankchat/data/irc/IrcMessageTest.kt
@@ -65,4 +65,22 @@ internal class IrcMessageTest {
         assertEquals(expected = "1594561955611", actual = ircMessage.tags["tmi-sent-ts"])
         assertEquals(expected = 4, actual = ircMessage.tags.size)
     }
+
+    @Test
+    fun `parse global user state message`() {
+        val msg = "@badge-info=;badges=;color=#19E6E6;display-name=randers;emote-sets=0,42,237;user-id=40286300;user-type= :tmi.twitch.tv GLOBALUSERSTATE"
+        val ircMessage = IrcMessage.parse(msg)
+
+        assertEquals(expected = "GLOBALUSERSTATE", actual = ircMessage.command)
+        assertEquals(expected = listOf(), actual = ircMessage.params)
+        assertEquals(expected = "tmi.twitch.tv", actual = ircMessage.prefix)
+        assertEquals(expected = "", actual = ircMessage.tags["badge-info"])
+        assertEquals(expected = "", actual = ircMessage.tags["badges"])
+        assertEquals(expected = "#19E6E6", actual = ircMessage.tags["color"])
+        assertEquals(expected = "randers", actual = ircMessage.tags["display-name"])
+        assertEquals(expected = "0,42,237", actual = ircMessage.tags["emote-sets"])
+        assertEquals(expected = "40286300", actual = ircMessage.tags["user-id"])
+        assertEquals(expected = "", actual = ircMessage.tags["user-type"])
+        assertEquals(expected = 7, actual = ircMessage.tags.size)
+    }
 }
