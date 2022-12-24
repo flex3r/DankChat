78acb6a2ddd29136d8c68948776109050622c1b7
Felix <felixschmidbb@gmail.com>
2022-12-24 15:55:33 +0100

    Add same handling for ffz & bttv

--- app/src/main/kotlin/com/flxrs/dankchat/data/api/bttv/BTTVApiClient.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/data/api/bttv/BTTVApiClient.kt
@@ -3,6 +3,7 @@ package com.flxrs.dankchat.data.api.bttv
 import com.flxrs.dankchat.data.UserId
 import com.flxrs.dankchat.data.api.bttv.dto.BTTVChannelDto
 import com.flxrs.dankchat.data.api.bttv.dto.BTTVGlobalEmoteDto
+import com.flxrs.dankchat.data.api.recoverNotFoundWith
 import com.flxrs.dankchat.data.api.throwApiErrorOnFailure
 import io.ktor.client.call.*
 import kotlinx.serialization.json.Json
@@ -12,11 +13,11 @@ import javax.inject.Singleton
 @Singleton
 class BTTVApiClient @Inject constructor(private val bttvApi: BTTVApi, private val json: Json) {
 
-    suspend fun getBTTVChannelEmotes(channelId: UserId): Result<BTTVChannelDto> = runCatching {
+    suspend fun getBTTVChannelEmotes(channelId: UserId): Result<BTTVChannelDto?> = runCatching {
         bttvApi.getChannelEmotes(channelId)
             .throwApiErrorOnFailure(json)
-            .body()
-    }
+            .body<BTTVChannelDto>()
+    }.recoverNotFoundWith(null)
 
     suspend fun getBTTVGlobalEmotes(): Result<List<BTTVGlobalEmoteDto>> = runCatching {
         bttvApi.getGlobalEmotes()
--- app/src/main/kotlin/com/flxrs/dankchat/data/api/ffz/FFZApiClient.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/data/api/ffz/FFZApiClient.kt
@@ -3,6 +3,7 @@ package com.flxrs.dankchat.data.api.ffz
 import com.flxrs.dankchat.data.UserId
 import com.flxrs.dankchat.data.api.ffz.dto.FFZChannelDto
 import com.flxrs.dankchat.data.api.ffz.dto.FFZGlobalDto
+import com.flxrs.dankchat.data.api.recoverNotFoundWith
 import com.flxrs.dankchat.data.api.throwApiErrorOnFailure
 import io.ktor.client.call.*
 import kotlinx.serialization.json.Json
@@ -12,11 +13,11 @@ import javax.inject.Singleton
 @Singleton
 class FFZApiClient @Inject constructor(private val ffzApi: FFZApi, private val json: Json) {
 
-    suspend fun getFFZChannelEmotes(channelId: UserId): Result<FFZChannelDto> = runCatching {
+    suspend fun getFFZChannelEmotes(channelId: UserId): Result<FFZChannelDto?> = runCatching {
         ffzApi.getChannelEmotes(channelId)
             .throwApiErrorOnFailure(json)
-            .body()
-    }
+            .body<FFZChannelDto>()
+    }.recoverNotFoundWith(null)
 
     suspend fun getFFZGlobalEmotes(): Result<FFZGlobalDto> = runCatching {
         ffzApi.getGlobalEmotes()
