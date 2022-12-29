13001435f704003ba2f34af660b91ba763e30adf
Felix <felixschmidbb@gmail.com>
2022-12-29 21:58:26 +0100

    Add some tests for duration parsing

--- app/src/test/kotlin/com/flxrs/dankchat/utils/DateTimeUtilsTest.kt
+++ app/src/test/kotlin/com/flxrs/dankchat/utils/DateTimeUtilsTest.kt
@@ -2,6 +2,7 @@ package com.flxrs.dankchat.utils
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 
 internal class DateTimeUtilsTest {
@@ -40,5 +41,65 @@ internal class DateTimeUtilsTest {
     fun `formats 1209600 seconds correctly`() {
         val result = DateTimeUtils.formatSeconds(1209600)
         assertEquals(expected = "14d", actual = result)
+    }
+
+    @Test
+    fun `returns null for blank input`() {
+        val result = DateTimeUtils.durationToSeconds("")
+        assertNull(result)
+    }
+
+    @Test
+    fun `returns null for invalid input`() {
+        val result = DateTimeUtils.durationToSeconds("d")
+        assertNull(result)
+    }
+
+    @Test
+    fun `parses number input correctly`() {
+        val result = DateTimeUtils.durationToSeconds("123")
+        assertEquals(expected = 123, actual = result)
+    }
+
+    @Test
+    fun `parses seconds input correctly`() {
+        val result = DateTimeUtils.durationToSeconds("123s")
+        assertEquals(expected = 123, actual = result)
+    }
+
+    @Test
+    fun `parses minutes input correctly`() {
+        val result = DateTimeUtils.durationToSeconds("2m")
+        assertEquals(expected = 120, actual = result)
+    }
+
+    @Test
+    fun `parses hours input correctly`() {
+        val result = DateTimeUtils.durationToSeconds("2h")
+        assertEquals(expected = 7200, actual = result)
+    }
+
+    @Test
+    fun `parses days input correctly`() {
+        val result = DateTimeUtils.durationToSeconds("2d")
+        assertEquals(expected = 172800, actual = result)
+    }
+
+    @Test
+    fun `parses weeks input correctly`() {
+        val result = DateTimeUtils.durationToSeconds("2w")
+        assertEquals(expected = 1209600, actual = result)
+    }
+
+    @Test
+    fun `parses combination of units correctly`() {
+        val result = DateTimeUtils.durationToSeconds("3s1h4d5m")
+        assertEquals(expected = 349503, actual = result)
+    }
+
+    @Test
+    fun `parses combination of units with spaces correctly`() {
+        val result = DateTimeUtils.durationToSeconds("3s 1h 4d 5m")
+        assertEquals(expected = 349503, actual = result)
     }
 }
