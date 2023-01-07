9bdb837ea8cab564a228c82592a54bd1da62cdad
Felix <felixschmidbb@gmail.com>
2023-01-07 21:26:59 +0100

    Use sdk constant

--- app/src/main/kotlin/com/flxrs/dankchat/utils/extensions/IntentExtensions.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/utils/extensions/IntentExtensions.kt
@@ -1,10 +1,10 @@
 package com.flxrs.dankchat.utils.extensions
 
 import android.content.Intent
-import android.os.Build.VERSION.SDK_INT
+import android.os.Build
 import android.os.Parcelable
 
 inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
-    SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
-    else          -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
+    else                                                  -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
 }
