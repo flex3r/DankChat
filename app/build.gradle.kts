37ebb5fa4265ddc0d97ff08e1f11d45940f86417
Felix <felixschmidbb@gmail.com>
2023-02-05 15:06:58 +0100

    Release 3.5.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.flxrs.dankchat"
         minSdk = 21
         targetSdk = 33
-        versionCode = 30509
-        versionName = "3.5.9"
+        versionCode = 30510
+        versionName = "3.5.10"
 
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
