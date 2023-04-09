d0481da339eee3bace601da96d579d6fa10e9395
Felix <felix.schmid@arconsis.com>
2023-04-09 20:32:20 +0200

    Bump alpha version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.flxrs.dankchat"
         minSdk = 21
         targetSdk = 33
-        versionCode = 30605
-        versionName = "3.6.5"
+        versionCode = 30606
+        versionName = "3.6.6"
 
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
