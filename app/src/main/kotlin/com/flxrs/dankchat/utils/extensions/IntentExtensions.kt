1433e3c8b4af02886d5481700c05a521bd2dde95
Felix <felixschmidbb@gmail.com>
2023-05-01 11:37:12 +0200

    Use compat method to parse Parcelable from intent, fixes crash on sdk 33

--- app/src/main/kotlin/com/flxrs/dankchat/utils/extensions/IntentExtensions.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/utils/extensions/IntentExtensions.kt
@@ -1,10 +1,8 @@
 package com.flxrs.dankchat.utils.extensions
 
 import android.content.Intent
-import android.os.Build
 import android.os.Parcelable
+import androidx.core.content.IntentCompat
 
-inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
-    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
-    else                                                  -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
-}
+// https://issuetracker.google.com/issues/240585930
+inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = IntentCompat.getParcelableExtra(this, key, T::class.java)
