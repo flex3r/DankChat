73dd1e43f91502eed0f72a278df1733ed65422ef
Felix <felixschmidbb@gmail.com>
2025-03-08 18:15:34 +0100

    Release 3.10.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.flxrs.dankchat"
         minSdk = 21
         targetSdk = 34
-        versionCode = 31004
-        versionName = "3.10.4"
+        versionCode = 31005
+        versionName = "3.10.5"
     }
 
     androidResources {
