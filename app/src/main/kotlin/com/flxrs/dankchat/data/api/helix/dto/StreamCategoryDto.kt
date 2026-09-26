6bc077ad062e44c926990b9dcbbffeb37bd2c299
3r01 <203520021+3r01@users.noreply.github.com>
2026-09-26 15:41:46 +0100

    feat: add stream title and game commands (#1236)
    Co-authored-by: Felix <felixschmidbb@gmail.com>

---
+++ app/src/main/kotlin/com/flxrs/dankchat/data/api/helix/dto/StreamCategoryDto.kt
@@ -0,0 +1,11 @@
+package com.flxrs.dankchat.data.api.helix.dto
+
+import androidx.annotation.Keep
+import kotlinx.serialization.Serializable
+
+@Keep
+@Serializable
+data class StreamCategoryDto(
+    val id: String,
+    val name: String,
+)
